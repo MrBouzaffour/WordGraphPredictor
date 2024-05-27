ff0c6895d60998a5eb4696d6e7e9f540c1d49982
ahmed <ahmedbouzaffour2002@gmail.com>
2024-05-27 15:37:27 +0000

    Documentation : Updated the documentation for the Vertex class

--- src/main/java/com/prediction/Graph/Vertex.java
+++ src/main/java/com/prediction/Graph/Vertex.java
@@ -1,22 +1,52 @@
 package main.java.com.prediction.Graph;
 import java.util.*;
 
-public class Vertex<V> implements Vertex_interface<V>{
-    private final V data;
-    private final List<Edge<V>> edges;
 
-    public Vertex(V data){
+/**
+* A Vertex class represents a vertex in a graph, holding the data associated with the vertex
+* and a list of edges connected to it.
+*/
+
+public class Vertex<V> implements Vertex_interface<V> {
+
+    private final V data;  // The data that will be stored in the vertex
+    private final List<Edge<V>> edges;  // List of all the edges for this vertex
+
+    /**
+     * Constructs a new vertex with the specified data.
+     *
+     * @param data the data to be stored in this vertex
+     */
+    public Vertex(V data) {
         this.data = data;
         this.edges = new ArrayList<>();
     }
+
+    /**
+     * Returns the data stored in this vertex.
+     *
+     * @return the data stored in this vertex
+     */
     @Override
     public V getData() {
         return data;
     }
+
+    /**
+     * Returns the list of edges connected to this vertex.
+     *
+     * @return a list of edges connected to this vertex
+     */
     @Override
     public List<Edge<V>> getEdges() {
         return edges;
     }
+
+    /**
+     * Adds an edge to the list of edges connected to this vertex.
+     *
+     * @param edge the edge to be added
+     */
     @Override
     public void addEdge(Edge<V> edge) {
         edges.add(edge);
