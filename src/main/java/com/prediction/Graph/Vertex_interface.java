862880f2830a3513a6632ef7debbdb19ce95c2b7
ahmed <ahmedbouzaffour2002@gmail.com>
2024-05-27 15:52:09 +0000

    Documentation : Updated the documentation of Vertex's interface

--- src/main/java/com/prediction/Graph/Vertex_interface.java
+++ src/main/java/com/prediction/Graph/Vertex_interface.java
@@ -2,7 +2,8 @@ package main.java.com.prediction.Graph;
 import java.util.List;
 
 public interface Vertex_interface<V> {
-    V getData();
-    List<Edge<V>> getEdges();
-    void addEdge(Edge<V> edge);
+
+    V getData();    // returns the data stored in this vertex
+    List<Edge<V>> getEdges();   // returns the list of edges connected to this vertex
+    void addEdge(Edge<V> edge);     // adds an edge to the list of edges connected to this vertex
 }
